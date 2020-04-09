39279a1b5b4016d8e593b183b148a1ac20d51e5e
zygui_g <zygui@163.com>
2020-04-09 20:54:14 +0800

    初始化stack,设计接口

---
+++ stack/src/com/zy/Main.java
@@ -0,0 +1,10 @@
+package com.zy;
+
+/**
+ * Description:
+ *
+ * @author zygui
+ * @date 2020/4/9 20:40
+ */
+public class Main {
+}
---
+++ stack/src/com/zy/Stack.java
@@ -0,0 +1,43 @@
+package com.zy;
+
+/**
+ * Description: 自定义栈结构
+ *
+ * @author zygui
+ * @date 2020/4/9 20:46
+ */
+public class Stack<E> {
+    // 接口设计
+    public int size() {
+        return 0;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    /**
+     * 压入元素
+     * @param element
+     */
+    public void push(E element) {
+
+    }
+
+    /**
+     * 弹出元素
+     * @return
+     */
+    public E pop() {
+        return null;
+    }
+
+    /**
+     * 获取栈顶元素
+     * @return
+     */
+    public E top() {
+        return null;
+    }
+
+}
